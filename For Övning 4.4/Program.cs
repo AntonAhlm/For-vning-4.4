5b1298a6056736537ae7778887a67c3fbbd4442e
Anton <>
2023-03-01 10:39:35 +0100

    Add project files.

---
+++ For Övning 4.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv in ett tal : ");
+            string text = Console.ReadLine();
+            int summa = 0;  
+            for(int i=0; i<text.Length;i++)
+            {
+                string textString = text[i].ToString();
+                int tal=Convert.ToInt32(textString);
+                summa += tal;
+
+            }
+            Console.WriteLine(summa);
+        }
+    }
+}
